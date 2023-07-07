2d9206f6b4882c6cf6493172b71269669d4c2290
bow1231 <bow1231@galaxia.co.kr>
2023-07-07 14:01:10 +0900

    환경변수 테스트 #5

--- utils/Axios.js
+++ utils/Axios.js
@@ -5,11 +5,7 @@ import * as Update from 'expo-updates';
 import { Platform } from 'react-native';
 
 export const baseAxios = axios.create({
-  baseURL: process.env.NODE_ENV=='development'
-    ? Platform.OS == 'ios'
-      ? 'http://127.0.0.1:8080'
-      : 'http://10.0.2.2:8080'
-    : process.env.EXPO_PUBLIC_BASE_API_URL,
+  baseURL: process.env.EXPO_PUBLIC_BASE_API_URL,
 });
 
 baseAxios.interceptors.request.use(async function (config) {
